a4a837065fb81638ce5104d13481cb439e1a9645
rukkiecodes <rukkiecodes@gmail.com>
2025-02-23 22:47:10 +0100

    done with post

--- src/pages/newPost.vue
+++ src/pages/newPost.vue
@@ -6,30 +6,29 @@
           Add a New News Reports
         </p>
         <p class="main-text text-body-2 text-sm-body-1 mt-2">
-          Streamline your project submission process and ensure your initiatives are part of the NDDC portfolio.
+          Streamline your project submission process and ensure your initiatives
+          are part of the NDDC portfolio.
         </p>
       </div>
 
       <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-        >
+        <v-col cols="12" sm="6">
           <div id="addPostIMage">
             <div class="d-flex mt-10 mb-5 cursor-pointer">
-              <span class="text-primary font-weight-bold text-h6 text-sm-h4 mr-3">#</span>
-              <p class="main-text text-h6 text-sm-h4 font-weight-bold text-grey-darken-2">
+              <span
+                class="text-primary font-weight-bold text-h6 text-sm-h4 mr-3"
+                >#</span
+              >
+              <p
+                class="main-text text-h6 text-sm-h4 font-weight-bold text-grey-darken-2"
+              >
                 Add post Images
               </p>
             </div>
 
             <v-row class="mt-10">
               <v-col cols="12">
-                <v-card
-                  height="500"
-                  rounded="lg"
-                  @click="pickBannerImage"
-                >
+                <v-card height="500" rounded="lg" @click="pickBannerImage" flat>
                   <v-card-text
                     class="d-flex justify-center align-center pa-0"
                     style="height: 100%"
@@ -39,7 +38,9 @@
                       :src="heroImage"
                       class="d-flex justify-center align-center text-center"
                     >
-                      <p class="block-text text-grey-darken-2 text-h6 text-sm-h4">
+                      <p
+                        class="block-text text-grey-darken-2 text-h6 text-sm-h4"
+                      >
                         Add banner image
                       </p>
                     </v-img>
@@ -68,14 +69,16 @@
           </div>
         </v-col>
 
-        <v-col
-          cols="12"
-          sm="6"
-        >
+        <v-col cols="12" sm="6">
           <div id="departmentsList">
             <div class="d-flex mt-10 mb-5 cursor-pointer">
-              <span class="text-primary font-weight-bold text-h6 text-sm-h4 mr-3">#</span>
-              <p class="main-text text-h6 text-sm-h4 font-weight-bold text-grey-darken-2">
+              <span
+                class="text-primary font-weight-bold text-h6 text-sm-h4 mr-3"
+                >#</span
+              >
+              <p
+                class="main-text text-h6 text-sm-h4 font-weight-bold text-grey-darken-2"
+              >
                 Select a department
               </p>
             </div>
@@ -93,7 +96,7 @@
             <div
               id="editor"
               class="border-md"
-              style="min-height: 200px; max-height: 500px;"
+              style="min-height: 200px; max-height: 500px"
             />
           </div>
         </v-col>
@@ -108,18 +111,28 @@
       rounded="pill"
       color="green-darken-3"
       :loading="loading"
+      :disabled="loading"
       @click="saveProject"
     >
       Save Project
     </v-btn>
+
+    <v-snackbar v-model="snackbar.show" :color="snackbar.color">
+      <p>{{ snackbar.text }}</p>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
-import { db } from '@/firebase';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import Quill from 'quill';
+import { db } from "@/firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
 export default {
@@ -139,24 +152,29 @@ export default {
       "Planning, Research, and Statistics",
       "Internal Audit",
       "Security Services",
-      "ICT and Innovation"
+      "ICT and Innovation",
     ],
-    department: 'Corporate Affairs Department',
+    department: "Corporate Affairs Department",
     articleDate: null,
     image: null,
     heroImage: null,
     isOngoing: false,
 
     quill: null,
-    loading: false
+    loading: false,
+    snackbar: {
+      show: false,
+      text: "",
+      color: "green",
+    },
   }),
 
-  mounted () {
+  mounted() {
     this.initQuill();
   },
 
   methods: {
-    pickBannerImage () {
+    pickBannerImage() {
       const input = document.createElement("input");
       input.type = "file";
       input.accept = "image/*";
@@ -166,7 +184,7 @@ export default {
 
         if (file) {
           const reader = new FileReader();
-          this.image = file
+          this.image = file;
 
           reader.onload = (e) => {
             this.heroImage = e.target.result;
@@ -179,64 +197,95 @@ export default {
       input.click();
     },
 
-    saveProject () {
-      if (!this.image) return alert("No image selected!")
-        
+    saveProject() {
+      if (!this.image) return alert("No image selected!");
+
+      const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB limit
+
+      if (this.image.size > MAX_FILE_SIZE) {
+        alert(
+          "File size exceeds the 2MB limit. Please select a smaller image."
+        );
+        return;
+      }
+
       if (this.quill) {
         const content = this.quill.root.innerHTML;
 
         try {
+          this.loading = true;
+
           // Initialize Firebase Storage
           const storage = getStorage();
-          const storageRef = ref(storage, `post-images/${this.image.name}/${new Date()}`);
+          const storageRef = ref(
+            storage,
+            `post-images/${this.image.name}/${new Date()}`
+          );
           const uploadTask = uploadBytesResumable(storageRef, this.image);
-
-          this.loading = true
 
           // Monitor the upload process
           uploadTask.on(
             "state_changed",
             (snapshot) => {
-              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
               console.log(`Upload is ${progress}% done`);
             },
             (error) => {
               console.error("Upload failed:", error);
+              this.snackbar = {
+                show: true,
+                text: "Upload failed. Please try again.",
+                color: "error",
+              };
+              this.loading = false;
             },
             async () => {
               // Upload completed, get the download URL
               const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
 
-              await addDoc(collection(db, 'posts'), {
+              await addDoc(collection(db, "posts"), {
                 department: this.department,
                 articleDate: this.articleDate,
                 isOngoing: this.isOngoing,
                 content,
-                image: { path: uploadTask.snapshot.ref.fullPath, uri: downloadURL },
-                timestamp: serverTimestamp()
-              })
+                image: {
+                  path: uploadTask.snapshot.ref.fullPath,
+                  uri: downloadURL,
+                },
+                timestamp: serverTimestamp(),
+              });
 
               // Save the download URL to your post data
               this.heroImage = downloadURL;
-              alert("Post uploaded successfully!");
-              this.loading = false
+              this.snackbar = {
+                show: true,
+                text: "Post uploaded successfully!",
+                color: "success",
+              };
+              this.loading = false;
             }
           );
-        } catch (error) {
-          console.error("Error uploading image:", error);
+        } catch (_) {
+          this.snackbar = {
+            show: true,
+            text: "Error uploading image. Please try again.",
+            color: "error",
+          };
+          this.loading = false;
         }
       }
     },
-
-    initQuill () {
+    
+    initQuill() {
       const toolbarOptions = [
-        ['bold', 'italic', 'underline', 'strike'],
-        ['blockquote'],
-        ['link'],
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote"],
+        ["link"],
 
-        [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
-        [{ indent: '-1' }, { indent: '+1' }],
-        [{ direction: 'rtl' }],
+        [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        [{ direction: "rtl" }],
         [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
         [{ color: [] }, { background: [] }],
@@ -247,15 +296,15 @@ export default {
         modules: {
           toolbar: toolbarOptions,
         },
-        placeholder: 'New post...',
-        theme: 'snow',
+        placeholder: "New post...",
+        theme: "snow",
       };
 
       // Initialize Quill and store the instance
-      this.quill = new Quill('#editor', options);
-    }
-  }
-}
+      this.quill = new Quill("#editor", options);
+    },
+  },
+};
 </script>
 
 <style></style>
